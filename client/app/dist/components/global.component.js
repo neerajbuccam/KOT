384c3dc175dbe9861abb2356377c223c6ee54c35
Neeraj Buccam <neerajhonda007@gmail.com>
2017-02-16 17:45:49 +0530

    deleted node_modules & other files

--- client/app/dist/components/global.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var GlobalComponent = (function () {
-    function GlobalComponent() {
-        this.tables = ['1', '2', '3', '4', '5', '6', 'Balcony'];
-    }
-    return GlobalComponent;
-}());
-exports.GlobalComponent = GlobalComponent;
-//# sourceMappingURL=global.component.js.map
