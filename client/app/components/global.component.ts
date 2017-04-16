3d820420c3aa27954d7d48ff1ceab42e937eac8f
Neeraj Buccam <neerajhonda007@gmail.com>
2017-04-15 18:16:21 -0700

    print align

--- client/app/components/global.component.ts
+++ client/app/components/global.component.ts
@@ -3,7 +3,7 @@ export class GlobalComponent{
     tables: string[];
 
     constructor(){
-        this.tables = ['1', '2', '3', '4', '5', '6', 'Balcony'];
+        this.tables = ['1', '2', '3', '4', '5', '6', '7', 'Balcony', 'Take Away'];
     }
     
 }
